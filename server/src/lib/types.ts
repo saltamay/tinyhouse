7c9d93f771a23b82036e48d8c91da24fadcea657
Sal Tamay <sal.tamay@outlook.com>
2021-08-24 00:02:59 -0400

    Implement database structure

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,5 +1,9 @@
 import { Collection, ObjectId } from 'mongodb';
 
+export interface Booking {
+  _id: ObjectId
+}
+
 export interface Listing {
 	_id: ObjectId;
 	title: string;
@@ -12,6 +16,12 @@ export interface Listing {
 	rating: number;
 }
 
+export interface User {
+  _id: ObjectId
+}
+
 export interface Database {
+  bookings: Collection<Booking>;
 	listings: Collection<Listing>;
+  users: Collection<User>;
 }
